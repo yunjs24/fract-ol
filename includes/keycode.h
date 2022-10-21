43b3d220185508487aeacf09c31e4011329f5f08
junsyun <yunjs24@gmail.com>
2022-10-22 04:51:59 +0900

    Add keycode.h

---
+++ includes/keycode.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keycode.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junsyun <junsyun@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/22 04:46:46 by junsyun           #+#    #+#             */
+/*   Updated: 2022/10/22 04:51:04 by junsyun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYCODE_H
+# define KEYCODE_H
+
+/* keyboard codes */
+# define KEY_ESC		53
+# define KEY_ENTER		36
+# define KEY_UP			125
+# define KEY_DOWN		123
+# define KEY_LEFT		123
+# define KEY_RIGHT		124
+
+/* mouse codes */
+# define M_LEFT_BUTTON	1
+# define M_SCROLL_UP	4
+# define M_SCROLL_DOWN	5
+
+#endif
