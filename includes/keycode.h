eca551bc46114e41ac3a442af84587cd15b0426c
junsyun <yunjs24@gmail.com>
2022-10-22 06:45:09 +0900

    fix keycode up down left right

--- includes/keycode.h
+++ includes/keycode.h
@@ -6,7 +6,7 @@
 /*   By: junsyun <junsyun@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 04:46:46 by junsyun           #+#    #+#             */
-/*   Updated: 2022/10/22 04:51:04 by junsyun          ###   ########.fr       */
+/*   Updated: 2022/10/22 06:44:40 by junsyun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 /* keyboard codes */
 # define KEY_ESC		53
 # define KEY_ENTER		36
-# define KEY_UP			125
-# define KEY_DOWN		123
+# define KEY_UP			126
+# define KEY_DOWN		125
 # define KEY_LEFT		123
 # define KEY_RIGHT		124
 
